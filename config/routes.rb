01ad061c96d8d80e03b1e081d94745b45ef69e5c
tigergm <39648421@qq.com>
2017-09-18 12:04:59 +0000

    加入钉钉审批

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-resources :corp_wechats 
+  # dingtalk router
+  get 'issues/:id/dingtalk_flow', :to => 'dingtalk#start_dingtalk_flow', :as => 'start_dingtalk_flow'
