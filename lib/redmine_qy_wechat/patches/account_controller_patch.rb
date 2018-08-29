ec5da7320ebb54fa22461e309a5d51ff38510334
tigergm <39648421@qq.com>
2018-08-29 12:09:33 +0000

    add the auto-login in dingtalk app

--- lib/redmine_qy_wechat/patches/account_controller_patch.rb
+++ lib/redmine_qy_wechat/patches/account_controller_patch.rb
@@ -13,97 +13,152 @@ module RedmineQyWechat
     
       module InstanceMethods
         def login_with_login_dingtalk
-          code = params[:code]
-          state = params[:state]
-          # 如果是钉钉登录回调
-          if state == "DingTalkSTATE"
-            appid = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appid"]
-            appsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appsecret"]
-            redirect_url = Setting["plugin_redmine_work_wechat"]["dingtalk_login_redirect"]
+          auth_code = params[:auth_code]
+          if auth_code.blank?
+            code = params[:code]
+            state = params[:state]
+            # 如果是钉钉登录回k调
+            if state == "DingTalkSTATE"
+              appid = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appid"]
+              appsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appsecret"]
+              redirect_url = Setting["plugin_redmine_work_wechat"]["dingtalk_login_redirect"]
             
-            if (appid.blank? || appsecret.blank? || redirect_url.blank?)
+              if (appid.blank? || appsecret.blank? || redirect_url.blank?)
+                return
+              end
+            
+              begin
+            
+              uri = URI.parse("https://oapi.dingtalk.com/sns/gettoken?appid=#{appid}&appsecret=#{appsecret}")
+          
+              http = Net::HTTP.new(uri.host, uri.port)
+              http.use_ssl = true
+              request_dd = Net::HTTP::Get.new(uri.request_uri)  
+          
+              response = http.request(request_dd)
+              
+              # 获得token
+              token = JSON.parse(response.body)["access_token"]
+          
+              uri = URI.parse("https://oapi.dingtalk.com/sns/get_persistent_code?access_token=#{token}&tmp_auth_code=#{code}")
+              http = Net::HTTP.new(uri.host,uri.port)
+              http.use_ssl = true
+          
+              data = {
+                "tmp_auth_code":  "#{code}"
+              }.to_json
+            
+              request_dd = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+          
+              request_dd.body = data
+          
+              response = http.request(request_dd)
+          
+          
+              # 获得openid和永久授权码
+              openid = JSON.parse(response.body)["openid"]
+              persistent_code = JSON.parse(response.body)["persistent_code"]
+          
+              uri = URI.parse("https://oapi.dingtalk.com/sns/get_sns_token?access_token=#{token}")
+          
+              http = Net::HTTP.new(uri.host,uri.port)
+              http.use_ssl = true
+          
+              data = {
+                "openid": "#{openid}",
+                "persistent_code": "#{persistent_code}"
+              }.to_json
+          
+              request_dd = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+          
+              request_dd.body = data
+              response = http.request(request_dd)
+            
+              # 获得sns_token
+              sns_token = JSON.parse(response.body)["sns_token"]
+          
+              uri = URI.parse("https://oapi.dingtalk.com/sns/getuserinfo?sns_token=#{sns_token}")
+          
+              http = Net::HTTP.new(uri.host,uri.port)
+              http.use_ssl = true
+              request_dd = Net::HTTP::Get.new(uri.request_uri)  
+          
+              response = http.request(request_dd)
+              
+              # 获得用户id
+              $dingid = JSON.parse(response.body)["user_info"]["dingId"]
+            
+              rescue
+              end
+            
+              user = User.find_by dingtalk_dingid: $dingid
+              if !user.blank?
+                if user.active?
+                  successful_authentication(user)
+                else
+                  handle_inactive_user(user)
+                end
+              else
+                if !$dingid.blank?
+                  flash[:notice] = l(:flash_dingtalk_bind)
+                end
+              end
+              return
+            end
+          else  # 处理钉钉免登
+            corpid = Setting["plugin_redmine_work_wechat"]["dingtalk_corp_id"]
+            corpsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_corp_secret"]
+            if (corpid.blank? || corpsecret.blank?)
+              flash[:error] = l(:flash_dingtalk_autologin_error)
+              redirect_to home_url
               return
             end
             
             begin
-            
-            uri = URI.parse("https://oapi.dingtalk.com/sns/gettoken?appid=#{appid}&appsecret=#{appsecret}")
+              uri = URI.parse("https://oapi.dingtalk.com/gettoken?corpid=#{corpid}&corpsecret=#{corpsecret}")
+              http = Net::HTTP.new(uri.host, uri.port)
+              http.use_ssl = true
+              request = Net::HTTP::Get.new(uri.request_uri)  
+              response = http.request(request)
           
-            http = Net::HTTP.new(uri.host, uri.port)
-            http.use_ssl = true
-            request_dd = Net::HTTP::Get.new(uri.request_uri)  
-          
-            response = http.request(request_dd)
+              # 获得token
+              token = JSON.parse(response.body)["access_token"]          
               
-            # 获得token
-            token = JSON.parse(response.body)["access_token"]
+              uri = URI.parse("https://oapi.dingtalk.com/user/getuserinfo?access_token=#{token}&code=#{auth_code}")
+              http = Net::HTTP.new(uri.host, uri.port)
+              http.use_ssl = true
+              request = Net::HTTP::Get.new(uri.request_uri)  
+              response = http.request(request)
           
-            uri = URI.parse("https://oapi.dingtalk.com/sns/get_persistent_code?access_token=#{token}&tmp_auth_code=#{code}")
-            http = Net::HTTP.new(uri.host,uri.port)
-            http.use_ssl = true
-          
-            data = {
-              "tmp_auth_code":  "#{code}"
-            }.to_json
-          
-            request_dd = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-          
-            request_dd.body = data
-          
-            response = http.request(request_dd)
-          
-          
-            # 获得openid和永久授权码
-            openid = JSON.parse(response.body)["openid"]
-            persistent_code = JSON.parse(response.body)["persistent_code"]
-          
-            uri = URI.parse("https://oapi.dingtalk.com/sns/get_sns_token?access_token=#{token}")
-          
-            http = Net::HTTP.new(uri.host,uri.port)
-            http.use_ssl = true
-          
-            data = {
-              "openid": "#{openid}",
-              "persistent_code": "#{persistent_code}"
-            }.to_json
-          
-            request_dd = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-          
-            request_dd.body = data
-            response = http.request(request_dd)
-          
-            # 获得sns_token
-            sns_token = JSON.parse(response.body)["sns_token"]
-          
-            uri = URI.parse("https://oapi.dingtalk.com/sns/getuserinfo?sns_token=#{sns_token}")
-          
-            http = Net::HTTP.new(uri.host,uri.port)
-            http.use_ssl = true
-            request_dd = Net::HTTP::Get.new(uri.request_uri)  
-          
-            response = http.request(request_dd)
+              # 获得token
               
-            # 获得用户id
-            $dingid = JSON.parse(response.body)["user_info"]["dingId"]
-            
+              err_code = JSON.parse(response.body)["errcode"]          
+              if err_code != 0
+                flash[:error] = l(:flash_dingtalk_autologin_error)
+                redirect_to home_url
+                return
+              end
+              
+              dingtalk_user_id = JSON.parse(response.body)["userid"]          
+               
             rescue
+              flash[:error] = l(:flash_dingtalk_autologin_error)
+              redirect_to home_url
+              return
             end
-            
-            user = User.find_by dingtalk_dingid: $dingid
-            if !user.blank?
-             if user.active?
+            user = User.find_by dingtalk_account_number: dingtalk_user_id
+            unless user.blank?
+              if user.active?
                 successful_authentication(user)
               else
                 handle_inactive_user(user)
               end
             else
-              if !$dingid.blank?
-                flash[:notice] = l(:flash_dingtalk_bind)
-              end
+              flash[:error] = l(:flash_dingtalk_autologin_error)
+              redirect_to home_url
             end
             return
           end
-          
           login_without_login_dingtalk
         end
       end
